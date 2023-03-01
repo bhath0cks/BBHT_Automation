c1f8e716f04b130023206384b47bf74d3b48fe28
Ahmad Raihan Prawira <69087709+raihansmart@users.noreply.github.com>
2023-03-01 03:35:21 -0500

    Adds JS for recon

--- BBH_Script.sh
+++ BBH_Script.sh
@@ -23,46 +23,25 @@ echo ""
 echo "${YELLOW} [!] Subdomain Enumeration and Probe is Starting ${RESET}"
 echo ""
  
-subfinder -d ${Domain} | httpx -mc 200 | tee $output/${Domain}_Subdomain_200.txt
-subfinder -d ${Domain} | httpx -mc 403 | tee $output/${Domain}_Subdomain_403.txt
+subfinder -d ${Domain} -nW -silent | httpx -silent | tee $output/${Domain}_Subdomain.txt
 
 echo ""
 echo "${GREEN} [+] Subdomain Enumeration and Probe has been Completed ${RESET}"
 echo ""
 echo "${GREEN} [+] Results are saved in $output directory ${RESET}"
-
-HostEnumeration
-SubdomainPermutation
-OriginIP
-Past_URL
 }
 
 # Finding Hosts through Shodan / Censys Enumreation via uncover
 
 HostEnumeration(){
 echo ""
-echo "${YELLOW} [!] Shodan or Censys Enumeration is Starting ${RESET}"
+echo "${YELLOW} [!] Shodan Enumeration is Starting ${RESET}"
 echo ""
 
-uncover -shodan 'ssl.cert.subject.CN:"${Domain}" 200' -shodan-idb 'ssl.cert.subject.CN:"${Domain}" 200' | httpx -mc 200 | tee -a $output/${Domain}_Host_Enumeration.txt
+uncover -shodan 'ssl.cert.subject.CN:"${Domain}" 200' -shodan-idb 'ssl.cert.subject.CN:"${Domain}" 200' -silent | httpx -mc 200 -silent | tee -a $output/${Domain}_Host_Enumeration.txt
 
 echo ""
-echo "${GREEN} [+] Shodan or Censys Enumereation has been Completed ${RESET}"
-echo ""
-echo "${GREEN} [+] Results are saved in $output directory ${RESET}"
-}
-
-# Perform Subdomain Permutations
-
-SubdomainPermutation(){
-echo ""
-echo "${YELLOW} [!] Subdomain Permutations is Starting ${RESET}"
-echo ""
-
-dnsgen -w /usr/share/wordlists/best-dns-wordlist.txt $output/${Domain}_Subdomain_200.txt | tee -a $output/${Domain}_Subdomain_Permutations.txt
-
-echo ""
-echo "${GREEN} [+] Subdomain Permutations has been Completed ${RESET}"
+echo "${GREEN} [+] Shodan Enumereation has been Completed ${RESET}"
 echo ""
 echo "${GREEN} [+] Results are saved in $output directory ${RESET}"
 }
@@ -74,10 +53,10 @@ echo ""
 echo "${YELLOW} [!] DNS Resolution is Starting ${RESET}"
 echo ""
 
-cat $output/${Domain}_Subdomain_200.txt | dnsx -l $output/${Domain}_Subdomain_200.txt -a -resp-only | sort -u | tee -a $output/${Domain}_OriginIP_200.txt
+cat $output/${Domain}_Subdomain.txt | dnsx -a -resp-only -t 1000 -silent -o $output/${Domain}_dnsx.txt
 
 echo ""
-for ip in $(cat $output/${Domain}_OriginIP_200.txt);do echo $ip && ffuf -w $output/${Domain}_Subdomain_200.txt -u http://$ip -H "Host: FUZZ" -s -mc 200; done | tee -a $output/${Domain}_OriginIP_200_Final.txt
+for ip in $(cat $output/${Domain}_dnsx.txt);do echo $ip && ffuf -w $output/${Domain}_Subdomain.txt -u http://$ip -H "Host: FUZZ" -s -mc 200; done | tee -a $output/${Domain}_OriginIP_Final.txt
 echo ""
 
 echo ""
@@ -93,7 +72,7 @@ echo ""
 echo "${YELLOW} [!] Finding Past / Known URL Enumeration is Starting ${RESET}"
 echo ""
 
-cat $output/${Domain}_Subdomain_200.txt | gau | tee -a $output/${Domain}_gau.txt
+cat $output/${Domain}_Subdomain.txt | gau | tee -a $output/${Domain}_gau.txt
 
 echo ""
 cat $output/${Domain}_gau.txt | uro > $output/${Domain}_final.txt
@@ -105,84 +84,83 @@ echo ""
 echo "${GREEN} [+] Results are saved in $output directory ${RESET}"
 }
 
+# Recon the JavaScript File
 
-# Searching Gf Pattern for Specific Vulnerabilities
+JavaScript(){
+# Gather JavaScript Files URL
+echo ""
+echo "${YELLOW} [!] Recon the JavaScript File is Starting ${RESET}"
+echo ""
+
+cat $output/${Domain}_final.txt | grep ".js$" | uniq | sort >> $output/${Domain}_jsfile_links.txt
+cat $output/${Domain}_Subdomain.txt | subjs >> $output/${Domain}_jsfile_links.txt
+cat $output/${Domain}_jsfile_links.txt | hakcheckurl | grep "200" | cut -d" " -f2 | sort -u > $output/${Domain}_live_jsfile_links.txt
+
+# Gather Endpoints From JavaScript Files
+
+cat $output/${Domain}_live_jsfile_links.txt | while read url; do python3 /home/kali/Tools/LinkFinder/linkfinder.py -d -i $url -o output_endpoints; done > $output/${Domain}_endpoints_jsfile_links.txt
+
+# Gather Secrets From JavaScript Files
+
+cat $output/${Domain}_live_jsfile_links.txt | while read url; do python3 //home/kali/Tools/SecretFinder/SecretFinder.py -i $url -o output_secret_js; done > $output/${Domain}_secrets_jsfile_links.txt
+
+# Collect Js Files For Manually Search
+mkdir $output/${Domain}/jsfiles 
+cp $output/${Domain}_live_jsfile_links.txt $output/${Domain}/jsfiles/hosts
+cd $output/${Domain}/jsfiles
+meg -d 1000 -v / 
+
+echo ""
+echo "${GREEN} [+] Recon the JavaScript File has been Completed ${RESET}"
+echo ""
+echo "${GREEN} [+] Results are saved in $output directory ${RESET}"	
+}
+
+
+# Using gf pattern to find Specific Vulnerabilities
 
 gfpattern(){
 	echo ""
-	echo "${YELLOW} [!] Searching gf pattern for Interesting JavaScript File ${RESET}"
-	cat $output/${Domain}_final.txt | gf js-interesting | tee $output/${Domain}_javascript.txt
-	echo ""
-	echo "${GREEN} [+] Searching gf pattern for Interesting JavaScript File has been Completed ${RESET}"
-
-	echo ""
-	echo "${YELLOW} [!] Searching gf pattern for Interesting Endpoints ${RESET}"
-	cat $output/${Domain}_final.txt | gf endpoints| tee $output/${Domain}_endpoints.txt
-	echo ""
-	echo "${GREEN} [+] Searching gf pattern for Interesting Endpoints has been Completed ${RESET}"
-
-	echo ""
 	echo "${YELLOW} [!] Searching gf pattern for Insecure Direct Object Reference Vulnerability (IDOR) ${RESET}"
-	cat $output/${Domain}_final.txt | gf idor | tee $output/${Domain}_idor.txt
+	cat $output/${Domain}_final.txt | gf idor | tee -a $output/${Domain}_idor.txt
 	echo ""
 	echo "${GREEN} [+] Searching gf pattern for Insecure Direct Object Reference Vulnerability (IDOR) has been Completed ${RESET}"
 
 	echo ""
 	echo "${YELLOW} [?] Searching gf pattern for Local File Inclusion Vulnerability (LFI) ${RESET}"
-	cat $output/${Domain}_final.txt | gf lfi | tee $output/${Domain}_lfi.txt
+	cat $output/${Domain}_final.txt | gf lfi | tee -a $output/${Domain}_lfi.txt
 	echo ""
 	echo "${GREEN} [+] Searching gf pattern for Local File Inclusion Vulnerability (LFI) has been Completed ${RESET}"
 
 	echo ""
 	echo "${YELLOW} [?] Searching gf pattern for Remote Code Execution Vulnerability (RCE) ${RESET}"
-	cat $output/${Domain}_final.txt | gf rce | tee $output/${Domain}_rce.txt
-	cat $output/${Domain}_final.txt | gf rce-2 | tee $output/${Domain}_rce_2.txt
-	cat $output/${Domain}_rce_2.txt | anew $output/${Domain}_rce.txt
-	rm -rf $output/${Domain}_rce_2.txt
+	cat $output/${Domain}_final.txt | gf rce | tee -a $output/${Domain}_rce.txt
 	echo ""
 	echo "${GREEN} [+] Searching gf pattern for Remote Code Execution Vulnerability (RCE) has been Completed ${RESET}"
 
 	echo ""
 	echo "${YELLOW} [?] Searching gf pattern for SQL Injection Vulnerability ${RESET}"
-	cat $output/${Domain}_final.txt | gf sqli | tee $output/${Domain}_sqli.txt
-	cat $output/${Domain}_final.txt | gf sqli-error | tee $output/${Domain}_sqli-error.txt
-	cat $output/${Domain}_sqli-error.txt | anew $output/${Domain}_sqli.txt
-	rm -rf $output/${Domain}_sqli-error.txt
+	cat $output/${Domain}_final.txt | gf sqli | tee -a $output/${Domain}_sqli.txt
 	echo ""
 	echo "${GREEN} [+] Searching gf pattern for SQL Injection Vulnerability has been Completed ${RESET}"
 
 	echo ""
 	echo "${YELLOW} [?] Searching gf pattern for Server-Side Request Forgery Vulnerability (SSRF) ${RESET}"
-	cat $output/${Domain}_final.txt | gf ssrf | tee $output/${Domain}_ssrf.txt
+	cat $output/${Domain}_final.txt | gf ssrf | tee -a $output/${Domain}_ssrf.txt
 	echo ""
 	echo "${GREEN} [+] Searching gf pattern for Server-Side Request Forgery Vulnerability (SSRF) has been Completed ${RESET}"
 
 	echo ""
 	echo "${YELLOW} [?] Searching gf pattern for Server-Side Template Injection Vulnerability (SSTI) ${RESET}"
-	cat $output/${Domain}_final.txt | gf ssti | tee $output/${Domain}_ssti.txt
+	cat $output/${Domain}_final.txt | gf ssti | tee -a $output/${Domain}_ssti.txt
 	echo ""
 	echo "${GREEN} [+] Searching gf pattern for Server-Side Template Injection Vulnerability (SSTI) has been Completed ${RESET}"
 
 	echo ""
 	echo "${YELLOW} [?] Searching gf pattern for Cross-site Scripting Vulnerability (XSS) ${RESET}"
-	cat $output/${Domain}_final.txt | gf xss | tee $output/${Domain}_xss.txt
-	cat $output/${Domain}_final.txt | gf domxss | tee $output/${Domain}_domxss.txt
-	cat $output/${Domain}_domxss.txt | anew $output/${Domain}_xss.txt
-	rm -rf $output/${Domain}_domxss.txt
+	cat $output/${Domain}_final.txt | gf xss | tee -a $output/${Domain}_xss.txt
 	echo ""
 	echo "${GREEN} [+] Searching gf pattern for Cross-site Scripting Vulnerability (XSS) has been Completed ${RESET}"
-
-	echo ""
-	echo "${YELLOW} [?] Searching gf pattern for Upload Fields Vulnerability ${RESET}"
-	cat $output/${Domain}_final.txt | gf upload-fields | tee $output/${Domain}_upload_fields.txt
-	echo ""
-	echo "${GREEN} [+] Searching gf pattern for Upload Fields Vulnerability has been Completed ${RESET}"
-
-	echo ""
-	echo "${YELLOW} [?] Searching gf pattern for XML External Entity Injection Vulnerability (XXE) ${RESET}"
-	cat $output/${Domain}_final.txt | gf xxe | tee $output/${Domain}_xxe.txt
-	echo ""
-	echo "${GREEN} [+] Searching gf pattern for XML External Entity Injection Vulnerability (XXE) has been Completed ${RESET}"
 }
 
 read -p "${YELLOW} [?] Enter your target domain: " Domain
@@ -195,6 +173,11 @@ then
 	mkdir -p $output
 	subdomain
 	
+	HostEnumeration
+	OriginIP
+	Past_URL
+	
+	JavaScript
 	gfpattern
 
 else
